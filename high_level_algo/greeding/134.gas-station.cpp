e0b255c28835f39542b5e3148238dfe59cf632e8
xuxumiao777 <107257126+xuxumiao777@users.noreply.github.com>
2022-12-11 13:21:01 +0100

    Create 134.gas-station.cpp

---
+++ high_level_algo/greeding/134.gas-station.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int curSum = 0;
+        int min = INT_MAX; // 从起点出发，油箱里的油量最小值
+        for (int i = 0; i < gas.size(); i++) {
+            int rest = gas[i] - cost[i];
+            curSum += rest;
+            if (curSum < min) {
+                min = curSum;
+            }
+        }
+        if (curSum < 0) return -1;  // 情况1
+        if (min >= 0) return 0;     // 情况2
+                                    // 情况3
+        for (int i = gas.size() - 1; i >= 0; i--) {
+            int rest = gas[i] - cost[i];
+            min += rest;
+            if (min >= 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
