a5b5d6b916690952f18fa1b693a3dc596282a62d
xuxumiao777 <xu.yan.tum@gmail.com>
2023-02-15 18:35:26 +0100

    15-02-2023

---
+++ 554.brick_wall.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        int n = wall.size();
+        unordered_map<int,int> map;
+        for (int i = 0, sum = 0; i < n; i++, sum = 0) {
+            for (int cur : wall[i]) {
+                sum += cur;
+                map[sum]++;
+            }
+            map.erase(sum); // 不能从两边穿过，需要 remove 掉最后一个
+        }
+        int ans = n;
+        for (auto& [u,cnt] : map) {
+            ans = min(ans, n - cnt);
+        }
+        return ans;
+    }
+};
