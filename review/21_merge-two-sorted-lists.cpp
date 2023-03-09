ec4c29c281a10fe5b20b32cdad0b991278e59b54
xuxumiao777 <xu.yan.tum@gmail.com>
2023-03-09 13:57:41 +0100

    09-03-2023

---
+++ review/21_merge-two-sorted-lists.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy=new ListNode(0),*node=dummy;
+        while(list1&&list2){
+            if(list1->val<=list2->val){
+                node->next=list1;
+                list1=list1->next;
+            }
+            else{
+                node->next=list2;
+                list2=list2->next;
+            }
+            node=node->next;
+            
+
+        }
+        node->next=list1?list1:list2;
+        return dummy->next;
+        
+    }
+};
+//
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* res=new ListNode(0);
+        ListNode* temp=res;
+        while(list1&&list2){
+            if(list1->val>=list2->val){
+                res->next=new ListNode(list2->val);
+                res=res->next;
+                list2=list2->next;
+            }
+            else{
+                res->next=new ListNode(list1->val);
+                res=res->next;
+                list1=list1->next;
+            }
+        }
+        if(list1){
+            res->next=list1;
+        }
+        if(list2){
+            res->next=list2;
+        }
+        return temp->next;
+    }
+};
