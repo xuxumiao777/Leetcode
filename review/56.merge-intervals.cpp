8d4f68d70811e65f956767752b47765b8380c30e
xuxumiao777 <xu.yan.tum@gmail.com>
2023-03-16 17:12:48 +0100

    16-03-2023

---
+++ review/56.merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool comp(vector<int> A,vector<int> B){
+        return A[0]<B[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),comp);
+        vector<vector<int>>res;
+        vector<int> temp=intervals[0];
+        for(auto cur:intervals){
+            if(cur[0]>temp[1]){
+                res.push_back(temp);
+                temp=cur;
+            }
+            else{
+                temp[1]=max(temp[1],cur[1]);
+            }
+        }
+        res.push_back(temp);
+        return res;
+    }
+};
